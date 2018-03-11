9c1c11515c98f24aa11f3ec60f82c484921ce17d
James Morris <blondfro@gmail.com>
2018-03-11 15:55:52 -0500

    created a guard for the product detail view to make sure that a number greater than 0 or is an actual number are valid before accessing the view.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {RouterModule} from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ProductDetailComponent } from './products/product-detail.component';
-import {WelcomeComponent} from './home/welcome.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
-import {ProductService} from './products/product.service';
+import { ProductService } from './products/product.service';
+import { ProductGuardService } from './products/product-guard.service';
 
 
 
@@ -33,14 +34,17 @@ import {ProductService} from './products/product.service';
     HttpClientModule,
     RouterModule.forRoot([
       {path: 'products', component: ProductListComponent },
-      {path: 'products/:id', component: ProductDetailComponent },
+      {path: 'products/:id',
+        canActivate:[ProductGuardService],
+        component: ProductDetailComponent },
       {path: 'welcome', component: WelcomeComponent },
       {path: '', redirectTo: 'welcome', pathMatch: 'full' },
       {path: '**', redirectTo: 'welcome', pathMatch: 'full' }
     ])
   ],
   providers: [
-    ProductService
+    ProductService,
+    ProductGuardService
   ],
   bootstrap: [AppComponent]
 })
